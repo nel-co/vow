794a03766ebfd949183a435eca319447a53bb2f3
Nelson Cothran <nelsonc19@Gmail.com>
2017-10-06 12:50:22 -0400

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var wait = require('gulp-wait');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
 gulp.task('styles', function () {
   return gulp.src('src/scss/styles.scss')
+    .pipe(wait(75))
     .pipe(sass())
-    .pipe(autoprefixer('last 2 version'))
+    // .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.reload({
       stream: true
